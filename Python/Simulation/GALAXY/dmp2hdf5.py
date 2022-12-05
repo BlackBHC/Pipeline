a6786264d29a2b2b9c5efd2a835cdb32f1cd6ec9
BlackBHC <blackbhc@icloud.com>
2022-12-05 14:31:36 +0800

    Initial GALAXY directory

---
+++ Python/Simulation/GALAXY/dmp2hdf5.py
@@ -0,0 +1,11 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import h5py
+from scipy.stats import binned_statistic as bin1d
+from scipy.stats import binned_statistic_2d as bin2d
+import imageio
+import sys
+import astropy
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+import random
